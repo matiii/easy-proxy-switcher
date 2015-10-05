80366d9a185aa224aef3fb00f3c768ba2bd469db
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2015-10-05 13:07:57 +0200

    Add proxy switcher logic
    Configuration come frome hard code

---
+++ src/EasyProxySwitcher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EasyProxySwitcher
+{
+    class Program
+    {
+        private static string proxy= "proxy.host:port";
+        private static string over = "don't use this sites;<local>";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start switching");
+            Console.WriteLine("Proxy: " + proxy);
+            Console.WriteLine("Wyjątki " + over);
+
+            try
+            {
+                var switcher = new ProxySwitcher(proxy, over);
+                switcher.Process();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR " + ex.Message);
+            }
+
+            Console.WriteLine("Switcher end.");
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/EasyProxySwitcher/ProxySwitcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace EasyProxySwitcher
+{
+    class ProxySwitcher
+    {
+        private readonly string _proxyHost;
+        private readonly string _proxyOverride;
+        private const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        private const int INTERNET_OPTION_REFRESH = 37;
+
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+
+        public ProxySwitcher(string proxyHost, string proxyOverride)
+        {
+            _proxyHost = proxyHost;
+            _proxyOverride = proxyOverride;
+        }
+
+        public void Process()
+        {
+            using (var registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true))
+            {
+                registry.DeleteValue("AutoConfigURL", false);
+                registry.SetValue("ProxyEnable", 1);
+                registry.SetValue("ProxyServer", _proxyHost);
+                registry.SetValue("ProxyOverride", _proxyOverride);
+            }
+
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+        }
+    }
+}
